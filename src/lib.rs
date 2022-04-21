fd477745bf6b54239833545e5923865b8b5ac5d3
driveways <1400055+GHDriveways@users.noreply.github.com>
2022-04-21 14:12:32 -0700

    Migrate code from cce-poll-tickers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use reqwest::header::{USER_AGENT, ACCEPT_ENCODING, HOST, HeaderMap};
+
+pub mod tickers;
+pub mod company_names;
+pub mod resources;
+
+pub fn sec_headers() -> HeaderMap {
+    let mut map = HeaderMap::new();
+    map.append(USER_AGENT, "Cash and Cash Equivalents admin@cashandcashequivalents.com".parse().unwrap());
+    map.append(ACCEPT_ENCODING, "gzip, deflate".parse().unwrap());
+    map.append(HOST, "www.sec.gov".parse().unwrap());
+    map
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
